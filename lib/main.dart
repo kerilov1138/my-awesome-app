7617f84e4221e1af3ae6e0633ca139c86dedfe17
kerilov1138 <aksahinkerem@gmail.com>
2026-01-28 11:23:58 +0300

    🚀 Build Trigger: Universal APK Generation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'dart:io';
 
 void main() {
-  runApp(const MaterialApp(home: WebViewApp(), debugShowCheckedModeBanner: false));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(
+    home: WebViewApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class WebViewApp extends StatefulWidget {
@@ -22,20 +25,12 @@ class _WebViewAppState extends State<WebViewApp> {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageFinished: (String url) {
-            debugPrint('Page finished loading: $url');
-          },
-        ),
-      )
       ..loadFlutterAsset('assets/www/index.html');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SafeArea(child: WebViewWidget(controller: controller)),
     );
   }
